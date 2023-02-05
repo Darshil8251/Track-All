31234eb2571e09b75a4f2304465ea41470efdf4c
Vishesh Agrawal <agrawalvishesh9271@gmail.com>
2023-02-05 21:57:21 +0530

    Sign In Working

--- src/components/Login/Singup.js
+++ src/components/Login/Singup.js
@@ -6,6 +6,7 @@ import {
   Route,
   Link,
   Routes,
+  Navigate,
 } from "react-router-dom";
 import axios from "axios";
 
@@ -18,15 +19,15 @@ function Signup() {
     password: "",
     ConfirmPassword: "",
   });
-  const [validate,setvalidate]=useState(false);
-  const [message , setMessage] = useState(false);
-  const [restaurantNameErrr , setRestaurantNameErrr] = useState(false);
-  const [mobileNoErrr , setMobileNoErrr] = useState(false);
-  const [emailErrr , setEmailErrr] = useState(false);
-  const [passErrr , setPassErrr] = useState(true);
-  const [cpassErrr , setCPassErrr] = useState(false);
-  const [Cpass , setCpass] = useState("");
-  const [pass,setPass] = useState("");
+  const [validate, setvalidate] = useState(false);
+  const [message, setMessage] = useState(false);
+  const [restaurantNameErrr, setRestaurantNameErrr] = useState(false);
+  const [mobileNoErrr, setMobileNoErrr] = useState(false);
+  const [emailErrr, setEmailErrr] = useState(false);
+  const [passErrr, setPassErrr] = useState(true);
+  const [cpassErrr, setCPassErrr] = useState(false);
+  const [Cpass, setCpass] = useState("");
+  const [pass, setPass] = useState("");
   // const storeData = () => {
   //   const Owner = document.getElementsByClassName("first-name-input").value;
   //   const Restaurent = document.getElementsByClassName("Restaurent-name").value;
@@ -40,13 +41,12 @@ function Signup() {
     // console.log("heyeyye",{...owner,[e.target.name]:e.target.value});
   };
   // const validateform=()=>{
-         
+
   //   const owner=owner.Name;
   //   const Rname=owner.RestaurantName;
   //   const email=owner.Email;
   //   const password=owner.password;
   //   const cpassword=owner.ConfirmPassword;
-
 
   //       if(owner==null){
   //         alert("Please Enter the Name:");
@@ -57,92 +57,73 @@ function Signup() {
   //       }
 
   // }
-  function nameHandle(e){
-    if(e.target.value.length<2 )
-    {
-        setMessage(true);
+  function nameHandle(e) {
+    if (e.target.value.length < 2) {
+      setMessage(true);
+    } else {
+      setMessage(false);
     }
-    else
-    {
-        setMessage(false);
-        
-    }
-    
-    
-    e.preventDefault();
-}
 
-  function restaurantNameHandle(e){
-    
-    if(e.target.value.length<1){
+    e.preventDefault();
+  }
+
+  function restaurantNameHandle(e) {
+    if (e.target.value.length < 1) {
       setRestaurantNameErrr(true);
-    }
-    else{
+    } else {
       setRestaurantNameErrr(false);
       // console.log(user);
-
     }
     e.preventDefault();
+  }
 
-}
-
-  function mobileNoHandle(e){
-
-    if(e.target.value.length == 10){
+  function mobileNoHandle(e) {
+    if (e.target.value.length == 10) {
       setMobileNoErrr(false);
-    }
-    else{
+    } else {
       setMobileNoErrr(true);
     }
     e.preventDefault();
   }
 
-  function passHandle(e){
+  function passHandle(e) {
     // console.log(e.target.value);
-    if(e.target.value.length > 8){
+    if (e.target.value.length > 8) {
       setPassErrr(false);
       setPass(e.target.value);
-      
-    }
-    else{
+    } else {
       setPassErrr(true);
     }
 
     e.preventDefault();
   }
 
- function cpassHandle(e){
-   
-  if(e.target.value == pass){
-    setCPassErrr(false);
-    setCpass(e.target.value);
+  function cpassHandle(e) {
+    if (e.target.value == pass) {
+      setCPassErrr(false);
+      setCpass(e.target.value);
+    } else {
+      setCPassErrr(true);
+    }
+    e.preventDefault();
   }
-  else{
-    setCPassErrr(true);
-  }
-  e.preventDefault();
- }
 
   const handlesubmit = (e) => {
     e.preventDefault();
-   
-     if(owner.Name==""){
+
+    if (owner.Name == "") {
       alert("Please Enter Restaurant Owner name");
       setvalidate(false);
-     }
-     else if(owner.RestaurantName==""){
+    } else if (owner.RestaurantName == "") {
       alert("Please Enter Correct Restaurent Name");
       setvalidate(false);
-     }
-     else if(owner.Email==""){
+    } else if (owner.Email == "") {
       alert("Please Enter email");
       setvalidate(false);
-     }
-     else if(owner.Number==""){
+    } else if (owner.Number == "") {
       alert("please Enter Phone Number");
       setvalidate(false);
-     }
-    else{
+    } else {
       setvalidate(true);
     }
     localStorage.setItem("Owner", JSON.stringify(owner));
@@ -195,11 +176,18 @@ function Signup() {
                 placeholder="Restaurent Owner Name"
                 name="Name"
                 value={owner.Owner}
-                onChange={e =>{handlechange(e) ;nameHandle(e) }}
+                onChange={(e) => {
+                  handlechange(e);
+                  nameHandle(e);
+                }}
                 required
               />
-               <br/>
-             {message?<span>Name is not valid(Contains more than 1 character)</span>:""}
+              <br />
+              {message ? (
+                <span>Name is not valid(Contains more than 1 character)</span>
+              ) : (
+                ""
+              )}
             </div>
             <div>
               <input
@@ -208,11 +196,20 @@ function Signup() {
                 placeholder="Restaurant Name"
                 name="RestaurantName"
                 value={owner.restaurent}
-                onChange={e =>{handlechange(e) ;restaurantNameHandle(e) }}
+                onChange={(e) => {
+                  handlechange(e);
+                  restaurantNameHandle(e);
+                }}
                 required
               />
-               <br/>
-              {restaurantNameErrr?<span>Restuarant name is not valid(Contains more than 1 character)</span>:""}
+              <br />
+              {restaurantNameErrr ? (
+                <span>
+                  Restuarant name is not valid(Contains more than 1 character)
+                </span>
+              ) : (
+                ""
+              )}
             </div>
 
             <div>
@@ -221,12 +218,19 @@ function Signup() {
                 className="mobile-no-input"
                 placeholder="Mobile No"
                 value={owner.mobile}
-                onChange={e =>{handlechange(e) ;mobileNoHandle(e) }}
+                onChange={(e) => {
+                  handlechange(e);
+                  mobileNoHandle(e);
+                }}
                 name="Number"
                 required
               />
-              <br/>
-          {mobileNoErrr ?<span>Mobile No is not valid(only 10 digit)</span>:""}
+              <br />
+              {mobileNoErrr ? (
+                <span>Mobile No is not valid(only 10 digit)</span>
+              ) : (
+                ""
+              )}
             </div>
 
             <div className="">
@@ -248,11 +252,20 @@ function Signup() {
                 name="password"
                 placeholder="Password"
                 value={owner.password}
-                onChange={e =>{handlechange(e) ;passHandle(e) }}
+                onChange={(e) => {
+                  handlechange(e);
+                  passHandle(e);
+                }}
                 required
               />
-               <br/>
-               {passErrr ? <span>Password must more than 8 digit or alphabate or symbol </span>:""}
+              <br />
+              {passErrr ? (
+                <span>
+                  Password must more than 8 digit or alphabate or symbol{" "}
+                </span>
+              ) : (
+                ""
+              )}
             </div>
             <div className="mb-3">
               <input
@@ -261,25 +274,33 @@ function Signup() {
                 placeholder="Confirm Password"
                 name="ConfirmPassword"
                 value={owner.cpassword}
-                onChange={e =>{handlechange(e) ;cpassHandle(e) }}
+                onChange={(e) => {
+                  handlechange(e);
+                  cpassHandle(e);
+                }}
                 required
               />
-              <br/>
-               {cpassErrr?<span>Confirm password not match</span>:"" }
+              <br />
+              {cpassErrr ? <span>Confirm password not match</span> : ""}
             </div>
-            <div> 
-              <button className="btncontinue" onClick={handlesubmit} >
-             
-              {
-                validate?<Link to="/setup" style={{textDecoration: 'none'}}>Continue</Link>:<Link to="/signup" style={{textDecoration: 'none'}}  >Continue</Link>
-              }
-              
+            <div>
+              <button className="btncontinue" onClick={handlesubmit}>
+                {validate ? (
+                  <Navigate to="/setup" />
+                ) : (
+                  <Link to="/signup" style={{ textDecoration: "none" }}>
+                    Continue
+                  </Link>
+                )}
               </button>
             </div>
             <hr />
             <div>
               <label className="signin-link">
-                Already have an account ? <Link to="signin" style={{textDecoration:'none'}} >Sign in</Link>
+                Already have an account ?{" "}
+                <Link to="signin" style={{ textDecoration: "none" }}>
+                  Sign in
+                </Link>
               </label>
             </div>
           </form>
